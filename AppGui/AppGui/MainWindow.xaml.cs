dc3f1653c7ceee73d95b4e812f76524afa08029a
António Ramos <tonyantonio@outlook.pt>
2022-05-02 19:27:29 +0100

    work

---
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+
+namespace AppGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MmiCommunication mmiC;
+
+        //  new 16 april 2020
+        private MmiCommunication mmiSender;
+        private LifeCycleEvents lce;
+        private MmiCommunication mmic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC.Message += MmiC_Message;
+            mmiC.Start();
+
+            // NEW 16 april 2020
+            //init LifeCycleEvents..
+            lce = new LifeCycleEvents("APP", "TTS", "User1", "na", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode
+            // MmiCommunication(string IMhost, int portIM, string UserOD, string thisModalityName)
+            mmic = new MmiCommunication("localhost", 8000, "User1", "GUI");
+            
+
+        }
+
+        private void MmiC_Message(object sender, MmiEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            var doc = XDocument.Parse(e.Message);
+            var com = doc.Descendants("command").FirstOrDefault().Value;
+            dynamic json = JsonConvert.DeserializeObject(com);
+
+            Shape _s = null;
+            switch ((string)json.recognized[0].ToString())
+            {
+                case "SQUARE": _s = rectangle;
+                    Vlc vlc = new Vlc();
+                    break;
+                case "CIRCLE": _s = circle;
+                    break;
+                case "TRIANGLE": _s = triangle;
+                    break;
+            }
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                switch ((string)json.recognized[1].ToString())
+                {
+                    case "GREEN":
+                        
+                        _s.Fill = Brushes.Green;
+                        break;
+                    case "BLUE":
+                        _s.Fill = Brushes.Blue;
+                        break;
+                    case "RED":
+                        _s.Fill = Brushes.Red;
+                        break;
+                }
+            });
+
+            //  new 16 april 2020
+            mmic.Send(lce.NewContextRequest());
+
+            string json2 = ""; // "{ \"synthesize\": [";
+            json2 += (string)json.recognized[0].ToString()+ " ";
+            json2 += (string)json.recognized[1].ToString() + " DONE." ;
+            //json2 += "] }";
+            /*
+             foreach (var resultSemantic in e.Result.Semantics)
+            {
+                json += "\"" + resultSemantic.Value.Value + "\", ";
+            }
+            json = json.Substring(0, json.Length - 2);
+            json += "] }";
+            */
+            var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, json2);
+            mmic.Send(exNot);
+
+
+        }
+    }
+}
---
+++ AppGui/AppGui/Vlc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGui
+{
+    internal class Vlc
+    {
+        public Vlc()
+        {
+
+            Process p;
+
+            p = new Process();
+            p.StartInfo.FileName = @"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe";
+            var DDIR = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine("Dir " + System.AppContext.BaseDirectory);
+            ///IM_FirstAssigment/AppGui/AppGui/bin/Debug/music
+            //
+
+            p.StartInfo.Arguments = "\"C:\\Users\\tonya\\Music\\Anthrax-GotTheTime.mp3\""; //C:\Users\tonya\Music\Anthrax-GotTheTime.mp3
+
+            //p.StartInfo.Arguments = System.AppContext.BaseDirectory + "\\music\\Anthrax-GotTheTime.mp3\""; //C:\Users\tonya\Music\Anthrax-GotTheTime.mp3
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+
+            p.OutputDataReceived += (sender2, e2) =>
+            {
+                if (e2.Data != null)
+                {
+                    Log("Received output data: " + e2.Data);
+                }
+                else
+                    Log("Received output data: NULL");
+            };
+
+            p.ErrorDataReceived += (sender2, e2) =>
+            {
+                if (e2.Data != null)
+                {
+                    Log("Received Error data: " + e2.Data);
+                }
+                else
+                    Log("Received Error data: NULL");
+            };
+
+            p.Start();
+
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+
+            p.StandardInput.WriteLine("get_time");
+            p.StandardInput.Flush();
+        }
+
+        private static void Log(string v)
+        {
+            Console.WriteLine(v);
+        }
+    
+    }
+}
